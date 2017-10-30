019d645b4e2ecdf51afe4979a7cf4e3f04bae09e
JangJinKyung <milkis95@ajou.ac.kr>
2017-10-30 19:45:30 +0900

    Update Canny_edge.py

--- vision/stair_case/Canny_edge.py
+++ vision/stair_case/Canny_edge.py
@@ -1,12 +1,19 @@
+import sys
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('asd.png', 0)
-edges = cv2.Canny(img, 100, 200)
+img = cv2.imread(sys.argv[1], 0)
+#amount of blur is controlled by third and forth parameters
+blur = cv2.bilateralFilter(img,5,60,60)
+edges = cv2.Canny(blur, 50, 100)
 
-plt.subplot(121), plt.imshow(img, cmap = 'gray')
-plt.title('Orgin'), plt.xticks([]), plt.yticks([])
+cv2.imwrite('blur.jpg', blur)
+cv2.imwrite('result.jpg', edges)
 
-plt.subplot(122), plt.imshow(edges, cmap = 'gray')
-plt.title('edge'), plt.xticks([]), plt.yticks([])
+#plt.subplot(121), plt.imshow(img, cmap = 'gray')
+#plt.title('Orgin'), plt.xticks([]), plt.yticks([])
+
+#plt.subplot(122), plt.imshow(blur, cmap = 'gray')
+#plt.title('filtered'), plt.xticks([]), plt.yticks([])
+
