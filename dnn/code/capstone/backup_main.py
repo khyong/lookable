f98b5d8567c2f8f8167c15caccdfbe9d61677575
khyong <khyong0101@gmail.com>
2017-12-10 13:56:25 +0900

    backup

--- dnn/code/capstone/backup_main.py
+++ dnn/code/capstone/backup_main.py
@@ -1,30 +1,33 @@
 import main_object_detection as od
 import threading
 
-IP = '0.0.0.0'
-
-PORT_PI = [9100, 9200, 9300]
-PORT_ULTRA = 10100
-PORT_GEO = 10200
+IP_PI = ['192.168.43.107', '192.168.43.204', '192.168.43.62']
+DIRECTION = ['front', 'diag', 'down']
+PORT_PI = 8666
+#PORT_ULTRA = 10100
+#PORT_GEO = 10200
 
 def main():
-  od.loadModel()
-  od.loadLabelMap()
-  sock_1, con_1 = connectPI(1)
-  sock_2, con_2 = connectPI(2)
-  sock_3, con_3 = connectPI(3)
-  t1 = threading.Thread(target=od.getImage, args=(sock_1, con_1, 'front'))
-  t2 = threading.Thread(target=od.getImage, args=(sock_2, con_2, 'diag'))
-  t3 = threading.Thread(target=od.getImage, args=(sock_3, con_3, 'down'))
+  cnt = 0
+  detection_graph = od.loadModel()
+  category_index = od.loadLabelMap()
+  cnt = connectPI(1, cnt, detection_graph, category_index)
+  #sock_2, con_2 = connectPI(2)
+  #sock_3, con_3 = connectPI(3)
+  #t1 = threading.Thread(target=od.getImage, args=(sock_1, con_1, 'front'))
+  #t2 = threading.Thread(target=od.getImage, args=(sock_2, con_2, 'diag'))
+  #t3 = threading.Thread(target=od.getImage, args=(sock_3, con_3, 'down'))
 
-  t1.start()
-  t2.start()
-  t3.start()
+  #t1.start()
+  #t2.start()
+  #t3.start()
 
-def connectPI(pi_num)
-  port = PORT_PI[pi_num-1]
-  client_sock, connection = od.setConnection(IP, port)
-  return client_sock, connection
+def connectPI(pi_num, cnt, detection_graph, category_index):
+  ip = IP_PI[pi_num-1]
+  direction = DIRECTION[pi_num-1]
+  cnt = od.getImage(ip, PORT_PI, direction, cnt, detection_graph, category_index)
+  print "[pi " + str(pi_num) + "] connected"
+  return cnt
 
 # android
 
